41b1ae4a2462916eb693a1e27e30de85b73fca25
dandugulaVenky <venkatdandugulayou@gmail.com>
2024-08-29 18:09:38 +0530

    changes done in playvideo page

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -6,7 +6,7 @@ import dislike from '../../assets/dislike.png'
 import share from '../../assets/share.png';
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
-import user_profile from '../../assets/user_profile.jpg'
+
 
 const playvideo = () => {
   return (
@@ -14,10 +14,10 @@ const playvideo = () => {
         <video src={video1} controls autoPlay muted ></video>
         <h3>Best YouTube Channel To Learn Web Development</h3>
         <div className='play-video-info'>
-            <p>1525 Views &bull; 2 days ago</p>
+            <p>15257 Views &bull; 8 days ago</p>
             <div>
                 <span><img src={like} alt="" />125</span>
-                <span><img src={dislike} alt="" />6</span>
+                <span><img src={dislike} alt="" />64</span>
                 <span><img src={share} alt="" />Share</span>
                 <span><img src={save} alt="" />Save</span>
             </div>
